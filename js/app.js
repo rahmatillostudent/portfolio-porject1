8d8ecb860c72655f0ea7d79670fac5829daa735d
rahmatillostudent <rahmatilloyoldashev.ry@gmail.com>
2024-03-21 02:51:44 +0300

    .

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const projects = [
+  {
+    title: "tipcalculator",
+    githubLink: " https://rahmatillostudent.github.io/TipCalculator/",
+    vercelLink: "tipcanculator.netlify.app",
+  },
+];
+
+const template = document.querySelector("template");
+const worksList = document.querySelector(".works__list");
+
+projects.forEach((project) => {
+  const clone = template.content.cloneNode(true);
+
+  const title = clone.querySelector(".works__list__title");
+  const linkGithub = clone.querySelector(".works__list__link__github");
+  const linkVercel = clone.querySelector(".works__list__link__vercel");
+
+  title.textContent = project.title;
+  linkGithub.href = project.githubLink;
+  linkVercel.href = project.vercelLink;
+
+  worksList.appendChild(clone);
+});
